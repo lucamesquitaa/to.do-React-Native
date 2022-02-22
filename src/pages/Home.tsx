7cfeb4c73ef610568451373b091f55ae24cd7cd6
Luca Mesquita <lucamesquitaa@gmail.com>
2022-02-22 20:00:38 -0300

    app to.do pronto - desafio concluido

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,16 +8,34 @@ import { TodoInput } from '../components/TodoInput';
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
+  interface Task {
+    id: number;
+    title: string;
+    done: boolean;
+  }
+
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const list = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+    setTasks(oldState => [...oldState, list])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedTasks = tasks.map(task => ({ ...task }));
+    const task = updatedTasks.find(a => a.id === id);
+    if(task){
+      task.done = !task.done;
+      setTasks(updatedTasks);
+    }
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(oldState => (oldState.filter(
+      task => task.id != id 
+    )));
   }
 
   return (
